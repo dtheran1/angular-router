60d0e70000f1e5d864fbedfcfa90d54f4b7d4abe
Dtheran <dtheran1@gmail.com>
2023-05-31 12:40:12 -0500

    lazy loading category module

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { MycartComponent } from './pages/mycart/mycart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -22,8 +21,8 @@ const routes: Routes = [
         component: HomeComponent,
       },
       {
-        path: 'category/:id',
-        component: CategoryComponent,
+        path: 'category',
+        loadChildren: () => import('./pages/category/category.module').then(m => m.CategoryModule)
       },
       {
         path: 'product/:id',
--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -4,7 +4,6 @@ import { SwiperModule } from 'swiper/angular';
 import { WebsiteRoutingModule } from './website-routing.module';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { MycartComponent } from './pages/mycart/mycart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -19,7 +18,6 @@ import { SharedModule } from './../shared/shared.module';
   declarations: [
     NavComponent,
     HomeComponent,
-    CategoryComponent,
     MycartComponent,
     LoginComponent,
     RegisterComponent,
